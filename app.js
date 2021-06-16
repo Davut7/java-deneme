b28351a240de74715e1d8563e0e71a37be7fa1a4
DavutSarısoy <davutsarisoy7@gmail.com>
2021-06-16 22:49:40 +0300

    js-deneme

---
+++ app.js
@@ -0,0 +1,45 @@
+// console.time();
+// console.log('Hello Clarusway');
+// console.info('some infor');
+// console.warn('some warning');
+// console.error('error !!!');
+ 
+ 
+//   console.log('------');
+//   var myName = 'Davut';
+//   console.log(myName);
+
+//   var num = 4;
+
+//   console.log(num * num);
+//   myName = 'Davut';
+//   console.log(myName)
+
+//  var announce = 'Welcome to our scholl!!!';
+//  console.log(announce);
+
+//  announce = 'Welcome to Full-Stack';
+//  console.log(announce);
+
+const PI = 3.141521;  // CONST DEĞERİ DEĞİŞTİRİLEMEZ...
+let myNum = 5;        // LET DEĞİŞTİRİLEBİLİR!!
+/* 
+console.log(PI);
+
+myNum = PI * 4;
+console.log(myNum);
+
+alert('MERHABA');
+
+var country = prompt('Enter your country :');
+
+console.log('Your country is :' , country); */
+
+var num1 = 42;
+if(num1 = 42) {
+    // var num2 = 22;
+    console.log(num1,num2);
+}
+
+var num2 = 88;
+console.log(num1,num2);
